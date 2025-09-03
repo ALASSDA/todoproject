9f08cb9c01c09b93b2a89064ccdc3c5834ee8fba
ALASSDA <nameni1383aa@gmail.com>
2025-09-03 12:49:35 -0700

    new

--- task/models.py
+++ task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    compeleted = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
          return self.title
